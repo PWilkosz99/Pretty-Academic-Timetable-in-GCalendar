fdc7dac830ddf509b14025f37171a561c5d9aed8
PWilkosz99 <pwilkosz99@gmail.com>
2021-02-12 22:51:04 +0100

    Make groups and datastring

--- timetable.py
+++ timetable.py
@@ -4,8 +4,23 @@ tt = 'timetable.xlsx'
 data = openpyxl.load_workbook(tt)
 sheet = data.active
 
-GK = [1,2]
+groups = ["GW01", "GA02", "GĆP02","GĆL03"]
 
-for row in sheet.rows:
-    for cell in row:
-        print(cell.value, end=" ")
+
+GW=[]
+GA=[]
+GP=[]
+GL=[]
+defs = [GW,GA,GP,GL]
+
+for i in range(4):
+    for row in sheet.rows:
+        if(row[9].value==groups[i]):
+            defs[i].append(row)
+
+
+for row in GP:
+    #row[2].value=row[2].value[:10]
+    print(row[0].value + "," + row[2].value[:10] + "," + str(row[3].value) + "," + row[2].value[:10] + "," + str(row[4].value) + "," + row[1].value + "," + row[5].value + ",,,,")
+
+
