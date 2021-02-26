e0273146a5eed1a76085bd547ed37a731951441a
PWilkosz99 <pwilkosz99@gmail.com>
2021-02-26 18:45:17 +0100

    Async generator

--- timetable.py
+++ timetable.py
@@ -5,18 +5,20 @@ tt = 'timetable.xlsx'
 wb = openpyxl.load_workbook(tt)
 sheet = wb.active
 
-groups = ["GW01", "GA02", "GĆP02","GĆL03"]
+groups = ["GW01", "GA02", "GĆP02","GĆL03","GCL_Now_04"]
 
 
 GW=[]
 GA=[]
 GP=[]
 GL=[]
-defs = [GW,GA,GP,GL]
+GSL=[]
+defs = [GW,GA,GP,GL,GSL]
+
 
 elrn= {"ABC ABC": "TEAMS", "DCE DCE": "UPEL"}
 
-for i in range(4):
+for i in range(5):
     for row in sheet.rows:
         if(row[9].value==groups[i]):
             defs[i].append(row)
@@ -64,19 +66,37 @@ if GLF.writable():
     GLF.write("Subject,Start Date,Start Time,End Date,End Time,Description,Location,,,,\n")
     for row in GL:
         GLF.write(row[0].value + "," + row[2].value[:10] + "," + str(row[3].value) + "," + row[2].value[:10] + "," + str(row[4].value) + "," + row[1].value + "," + room(row[5].value, row[1].value) + ",,,,\n")
+    for row in GSL:
+        GLF.write(row[0].value + "," + row[2].value[:10] + "," + str(row[3].value) + "," + row[2].value[:10] + "," + str(row[4].value) + "," + row[1].value + "," + room(row[5].value, row[1].value) + ",,,,\n")
 
 GLF.close()
 
 
 #other activities generator
 
+# GE = open("GE.csv", "w")
+# GEcounter = 15
+# deltadays=datetime.timedelta(0)
+# startdate=datetime.date(2021, 3, 1)
+# if GE.writable():
+#     GE.write("Subject,Start Date,Start Time,End Date,End Time,Description,Location,,,,\n")
+#     while GEcounter>0:
+#         GE.write("EXAMPLE SUBJECT,"+(str)(startdate+deltadays)+","+"16:00:00"+","+(str)(startdate+deltadays)+","+"17:30:00"+","+"EXAMPLE TEACHER"+","+"ONLINE"+",,,,\n")
+#         deltadays=deltadays+datetime.timedelta(7)
+#         GEcounter=GEcounter-1
+
+#async version
+
 GE = open("GE.csv", "w")
-GEcounter = 15
+GEcounter = 30
 deltadays=datetime.timedelta(0)
 startdate=datetime.date(2021, 3, 1)
 if GE.writable():
     GE.write("Subject,Start Date,Start Time,End Date,End Time,Description,Location,,,,\n")
     while GEcounter>0:
         GE.write("EXAMPLE SUBJECT,"+(str)(startdate+deltadays)+","+"16:00:00"+","+(str)(startdate+deltadays)+","+"17:30:00"+","+"EXAMPLE TEACHER"+","+"ONLINE"+",,,,\n")
-        deltadays=deltadays+datetime.timedelta(7)
+        if(GEcounter%2==0):
+            deltadays=deltadays+datetime.timedelta(2)
+        else:
+            deltadays=deltadays+datetime.timedelta(5)
         GEcounter=GEcounter-1
